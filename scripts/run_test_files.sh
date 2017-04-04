ead87882cfb9f2af3f7ff5e639e662393b403a65
Bastian Löher <b.loeher@gsi.de>
2017-04-04 12:09:38 +0200

    Rename variable.

--- scripts/run_test_files.sh
+++ scripts/run_test_files.sh
@@ -14,5 +14,5 @@ function aterror {
 trap aterror ERR
 EOF
 
-awk -vpath=$R3B_DATA_PATH '$1!~/^#/{print "./"$1, path"/"$2, "--max-events=1000"}' unpacker_lmd_corr >> $OUT
+awk -vpath=$TEST_DATA_PATH '$1!~/^#/{print "./"$1, path"/"$2, "--max-events=1000"}' unpacker_lmd_corr >> $OUT
 chmod +x $OUT
