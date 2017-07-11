c8d3931afb5cd029bcdde7f9457d04123d8891da
Bastian Löher <b.loeher@gsi.de>
2017-07-11 06:12:35 +0200

    Fix awk script sigbeam2signal.sh for generating neuland mapping. Now also works with old awk versions.

--- s438b/sigbeam2signal.sh
+++ s438b/sigbeam2signal.sh
@@ -10,10 +10,10 @@ awk 'NR>1{sam=substr($16,4,1);
 	bar=substr($4,7,2);
 	pm=substr($2,10,1);
 	signal=plane"_"pm"_";
-	if (found[plane][pm] == 0) {
+	if (found[plane,pm] == 0) {
 		print "SIGNAL(ZERO_SUPPRESS: "signal"1);";
 	}
-	found[plane][pm]=1;
+	found[plane,pm]=1;
 	signal=signal bar;
 	print "SIGNAL("signal"TAC, neuland_tacq.data_sam"sam"_gtb"gtb".tac["int(addr-1)"]["chm1"], DATA12);";
 	print "SIGNAL("signal"CLK, neuland_tacq.data_sam"sam"_gtb"gtb".clk["int(addr-1)"]["chm1"], DATA12);";
