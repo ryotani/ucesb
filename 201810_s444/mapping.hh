dd29be6c713701ec1c586b003501b674babd2089
Haik Simon <land@lx-pool.gsi.de>
2018-06-28 08:51:13 +0200

    Automatically generate gen/mapping_tofd.hh.

--- 201810_s444/mapping.hh
+++ 201810_s444/mapping.hh
@@ -1,4 +1,4 @@
 #include "mapping_timestamp.hh"
 #include "mapping_los.hh"
 #include "mapping_fiber.hh"
-#include "mapping_tofd.hh"
+#include "gen/mapping_tofd.hh"
--- 201810_s444/mapping_tofd_gen.bash
+++ 201810_s444/mapping_tofd_gen.bash
@@ -1,19 +1,47 @@
 #!/bin/bash
 
-echo "// $(date)" > mapping_tofd.hh
-echo "SIGNAL(ZERO_SUPPRESS_MULTI(10): TOFD_TTFL1);" >> mapping_tofd.hh
-echo "SIGNAL(ZERO_SUPPRESS_MULTI(10): TOFD_TTFT1);" >> mapping_tofd.hh
-echo "SIGNAL(ZERO_SUPPRESS_MULTI(10): TOFD_TTCL1);" >> mapping_tofd.hh
-echo "SIGNAL(ZERO_SUPPRESS_MULTI(10): TOFD_TTCT1);" >> mapping_tofd.hh
-ofs=1
-for tamex in $(seq 0 3)
+#
+# s444
+#
+# 2 planes 20 paddles -> 5 tamex cards
+# BL: Currently only 4 tamex cards used, so stop generating after 4 cards
+#
+
+#
+# s454
+#
+# 2 planes 44 paddles -> 11 tamex cards
+#
+
+echo "// $0 on $(date)"
+tamex=0
+ch=0
+for plane in 1 2
 do
-	for ch in $(seq 1 16)
+	for side in 1 2
 	do
-		echo "SIGNAL(TOFD_TTFL$ofs,tofd_tamex.tamex[$tamex].time_fine[$((2*ch-1))], DATA12);" >> mapping_tofd.hh
-		echo "SIGNAL(TOFD_TTFT$ofs,tofd_tamex.tamex[$tamex].time_fine[$((2*ch))], DATA12);" >> mapping_tofd.hh
-		echo "SIGNAL(TOFD_TTCL$ofs,tofd_tamex.tamex[$tamex].time_coarse[$((2*ch-1))], DATA12);" >> mapping_tofd.hh
-		echo "SIGNAL(TOFD_TTCT$ofs,tofd_tamex.tamex[$tamex].time_coarse[$((2*ch))], DATA12);" >> mapping_tofd.hh
-		ofs=$((ofs+1))
+		echo "SIGNAL(ZERO_SUPPRESS_MULTI(10): TOFD_P${plane}T${side}_TFL1);"
+		echo "SIGNAL(ZERO_SUPPRESS_MULTI(10): TOFD_P${plane}T${side}_TFT1);"
+		echo "SIGNAL(ZERO_SUPPRESS_MULTI(10): TOFD_P${plane}T${side}_TCL1);"
+		echo "SIGNAL(ZERO_SUPPRESS_MULTI(10): TOFD_P${plane}T${side}_TCT1);"
+	done
+	for paddle in $(seq 1 20)
+	do
+		for side in 1 2
+		do
+			echo "SIGNAL(TOFD_P${plane}T${side}_TFL${paddle},tofd_tamex.tamex[$tamex].time_fine[$((2*ch+1))], DATA12);"
+			echo "SIGNAL(TOFD_P${plane}T${side}_TFT${paddle},tofd_tamex.tamex[$tamex].time_fine[$((2*ch+2))], DATA12);"
+			echo "SIGNAL(TOFD_P${plane}T${side}_TCL${paddle},tofd_tamex.tamex[$tamex].time_coarse[$((2*ch+1))], DATA12);"
+			echo "SIGNAL(TOFD_P${plane}T${side}_TCT${paddle},tofd_tamex.tamex[$tamex].time_coarse[$((2*ch+2))], DATA12);"
+			ch=$((ch+1))
+			if [ $ch -eq 16 ]
+			then
+				tamex=$((tamex+1))
+				if [ $tamex -eq 4 ]; then
+					exit;
+				fi
+				ch=0
+			fi
+		done
 	done
 done
