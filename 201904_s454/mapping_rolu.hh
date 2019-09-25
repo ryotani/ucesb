000fc4de1ccbe5e7872d1bf23373662dc858178b
Haik Simon <land@lx-pool.gsi.de>
2019-09-25 11:53:16 +0200

    Mapping for ROLU 2.

--- 201904_s454/mapping_rolu.hh
+++ 201904_s454/mapping_rolu.hh
@@ -1,6 +1,8 @@
 // ROLU FQT/TAMEX3 fine (TDC) values.
 SIGNAL(ZERO_SUPPRESS_MULTI(10): ROLU1_TTFL1);
 SIGNAL(ZERO_SUPPRESS_MULTI(10): ROLU1_TTFT1);
+SIGNAL(ZERO_SUPPRESS_MULTI(10): ROLU2_TTFL1);
+SIGNAL(ZERO_SUPPRESS_MULTI(10): ROLU2_TTFT1);
 SIGNAL(ROLU1_TTFL1, fib_tamex.data.tamex[3].time_fine[17], DATA12);
 SIGNAL(ROLU1_TTFT1, fib_tamex.data.tamex[3].time_fine[18], DATA12);
 SIGNAL(ROLU1_TTFL2, fib_tamex.data.tamex[3].time_fine[19], DATA12);
@@ -9,10 +11,20 @@ SIGNAL(ROLU1_TTFL3, fib_tamex.data.tamex[3].time_fine[21], DATA12);
 SIGNAL(ROLU1_TTFT3, fib_tamex.data.tamex[3].time_fine[22], DATA12);
 SIGNAL(ROLU1_TTFL4, fib_tamex.data.tamex[3].time_fine[23], DATA12);
 SIGNAL(ROLU1_TTFT4, fib_tamex.data.tamex[3].time_fine[24], DATA12);
+SIGNAL(ROLU2_TTFL1, fib_tamex.data.tamex[3].time_fine[25], DATA12);
+SIGNAL(ROLU2_TTFT1, fib_tamex.data.tamex[3].time_fine[26], DATA12);
+SIGNAL(ROLU2_TTFL2, fib_tamex.data.tamex[3].time_fine[27], DATA12);
+SIGNAL(ROLU2_TTFT2, fib_tamex.data.tamex[3].time_fine[28], DATA12);
+SIGNAL(ROLU2_TTFL3, fib_tamex.data.tamex[3].time_fine[29], DATA12);
+SIGNAL(ROLU2_TTFT3, fib_tamex.data.tamex[3].time_fine[30], DATA12);
+SIGNAL(ROLU2_TTFL4, fib_tamex.data.tamex[3].time_fine[31], DATA12);
+SIGNAL(ROLU2_TTFT4, fib_tamex.data.tamex[3].time_fine[32], DATA12);
 
 // ROLU FQT/TAMEX3 coarse (clock) values.
 SIGNAL(ZERO_SUPPRESS_MULTI(10): ROLU1_TTCL1);
 SIGNAL(ZERO_SUPPRESS_MULTI(10): ROLU1_TTCT1);
+SIGNAL(ZERO_SUPPRESS_MULTI(10): ROLU2_TTCL1);
+SIGNAL(ZERO_SUPPRESS_MULTI(10): ROLU2_TTCT1);
 SIGNAL(ROLU1_TTCL1, fib_tamex.data.tamex[3].time_coarse[17], DATA12);
 SIGNAL(ROLU1_TTCT1, fib_tamex.data.tamex[3].time_coarse[18], DATA12);
 SIGNAL(ROLU1_TTCL2, fib_tamex.data.tamex[3].time_coarse[19], DATA12);
@@ -21,3 +33,11 @@ SIGNAL(ROLU1_TTCL3, fib_tamex.data.tamex[3].time_coarse[21], DATA12);
 SIGNAL(ROLU1_TTCT3, fib_tamex.data.tamex[3].time_coarse[22], DATA12);
 SIGNAL(ROLU1_TTCL4, fib_tamex.data.tamex[3].time_coarse[23], DATA12);
 SIGNAL(ROLU1_TTCT4, fib_tamex.data.tamex[3].time_coarse[24], DATA12);
+SIGNAL(ROLU2_TTCL1, fib_tamex.data.tamex[3].time_coarse[25], DATA12);
+SIGNAL(ROLU2_TTCT1, fib_tamex.data.tamex[3].time_coarse[26], DATA12);
+SIGNAL(ROLU2_TTCL2, fib_tamex.data.tamex[3].time_coarse[27], DATA12);
+SIGNAL(ROLU2_TTCT2, fib_tamex.data.tamex[3].time_coarse[28], DATA12);
+SIGNAL(ROLU2_TTCL3, fib_tamex.data.tamex[3].time_coarse[29], DATA12);
+SIGNAL(ROLU2_TTCT3, fib_tamex.data.tamex[3].time_coarse[30], DATA12);
+SIGNAL(ROLU2_TTCL4, fib_tamex.data.tamex[3].time_coarse[31], DATA12);
+SIGNAL(ROLU2_TTCT4, fib_tamex.data.tamex[3].time_coarse[32], DATA12);
